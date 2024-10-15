6323e777900e2b565128c93338befa49fcbf151d
Denis Mugambi Muthuri <mugambimuthuri0@gmail.com>
2024-10-15 21:40:25 +0300

    Project Setup and Initial Commit

---
+++ components/Hero.jsx
@@ -0,0 +1,10 @@
+"use client";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import { Link as ScrollLink } from "react-scroll";
+
+const Hero = () => {
+  return <section>hero</section>
+}
+
+export default Hero
